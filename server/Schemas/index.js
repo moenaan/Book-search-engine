5b85019ec59a6da40596a2f4fed71967a8f88f5c
Mohammed Samod <mohammedlee27@gmail.com>
2021-11-19 21:23:51 -0600

    update code

--- server/Schemas/index.js
+++ server/Schemas/index.js
@@ -1,20 +1,25 @@
-const express = require("express");
-const app = express();
-const PORT = 3001;
-const { graphqlHTTP } = require("express-graphql");
-const schema = require("../Schemas/index");
-const cors = require("cors");
+// const express = require("express");
+// const app = express();
+// const PORT = 3001;
+// const { graphqlHTTP } = require("express-graphql");
+// const schema = require("../Schemas/index");
+// const cors = require("cors");
 
-app.use(cors());
-app.use(express.json());
-app.use(
-    "/graphql",
-    graphqlHTTP({
-        schema,
-        graphiql: true,
-    })
-);
+// app.use(cors());
+// app.use(express.json());
+// app.use(
+//     "/graphql",
+//     graphqlHTTP({
+//         schema,
+//         graphiql: true,
+//     })
+// );
 
-app.listen(PORT, () => {
-    console.log("server running");
-});
+// app.listen(PORT, () => {
+//     console.log("server running");
+// });
+
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+module.exports = { typeDefs, resolvers };
